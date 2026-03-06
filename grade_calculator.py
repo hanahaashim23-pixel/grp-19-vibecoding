d5d1606018e6b2d0c3244b12678374e1dba1d783
hanahaashim23-pixel <hanahaashim23@gmail.com>
2026-03-06 14:34:28 +0530

    updated to loop

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,21 +1,28 @@
-# Grade Calculator with Grades
+# Grade Calculator with Continuous Execution
 
-name = input("Enter student's name: ")
+while True:
+    name = input("Enter student's name (type 'Exit' to stop): ")
+    
+    if name.lower() == "exit":
+        print("Program ended.")
+        break
 
-mark1 = float(input("Enter mark for subject 1: "))
-mark2 = float(input("Enter mark for subject 2: "))
-mark3 = float(input("Enter mark for subject 3: "))
+    mark1 = float(input("Enter mark for subject 1: "))
+    mark2 = float(input("Enter mark for subject 2: "))
+    mark3 = float(input("Enter mark for subject 3: "))
 
-average = (mark1 + mark2 + mark3) / 3
+    average = (mark1 + mark2 + mark3) / 3
 
-print("Student Name:", name)
-print("Average:", average)
+    print("Student Name:", name)
+    print("Average:", average)
 
-if average >= 75:
-    print("Grade: A")
-elif average >= 60:
-    print("Grade: B")
-elif average >= 40:
-    print("Grade: C")
-else:
-    print("Result: Fail")
+    if average >= 75:
+        print("Grade: A")
+    elif average >= 60:
+        print("Grade: B")
+    elif average >= 40:
+        print("Grade: C")
+    else:
+        print("Result: Fail")
+
+    print("---------------------------")
